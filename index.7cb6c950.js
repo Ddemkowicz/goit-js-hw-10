01c67597dc48afd3d9b89e6c34c0091d53284732
Ddemkowicz <114504693+Ddemkowicz@users.noreply.github.com>
2023-03-21 12:55:17 +0000

    Deploying to gh-pages from @ Ddemkowicz/goit-js-hw-10@7f907c906ecd3c2bfa876d395fe767f4de329834 🚀

--- index.7cb6c950.js
+++
@@ -1,2 +0,0 @@
-const e=console.log,t=document.querySelector("#search-box");document.querySelector(".country-list");document.querySelector("button").addEventListener("click",(()=>{e(t.value)})),t.addEventListener("input",(function n(c){t.value=c,fetch("https://restcountries.com/v3.1/name/{name}"),e("fetchCountries: ",n)}));
-//# sourceMappingURL=index.7cb6c950.js.map
